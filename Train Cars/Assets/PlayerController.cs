a71c1bb4f8ac7e88a137592ea5beabe4644bd80d
MageyMage <64452472+MageyMage@users.noreply.github.com>
2020-10-02 17:36:16 -0700

    added a "script"

---
+++ Train Cars/Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0)){
+             //rotato stuff to the right or left mouse
+        };
+           
+        if(Input.GetMouseButtonDown(1)){
+            //rotato stuff to the left or left mouse
+        };
+    }
+}
