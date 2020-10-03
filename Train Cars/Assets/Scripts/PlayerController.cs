6c6f00a752842421e5a4858b22c683b27365bbbe
MageyMage <64452472+MageyMage@users.noreply.github.com>
2020-10-03 11:48:40 -0700

    collision actaully does stuff now

--- Train Cars/Assets/Scripts/PlayerController.cs
+++ Train Cars/Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
@@ -39,6 +40,7 @@ public class PlayerController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other)
     {
        Debug.Log("Hit");
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
     }
 
     void RotateLeft () {
